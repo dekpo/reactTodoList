54496a99029572a8d7f38e0d7b81166621de4fdc
Dekpo-Wyna Yologaza <dekpo@me.com>
2020-06-01 20:07:39 +0200

    Ajout du module react-router-dom pour intégrer un composant AddTask et ajout de liens NavLink sur la NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import TodoList from './TodoList';
 import NavBar from './NavBar';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import AddTask from './AddTask';
 
 class App extends React.Component{
     render(){
       return(
-            <section id="todo">
-            <TodoList />
-            <NavBar />
-        </section>     
+          <section id="todo">
+            <BrowserRouter>
+              <Switch>
+                <Route path="/add-task" component={AddTask} />
+                <Route path="/:filter?" component={TodoList} />
+              </Switch>
+              <NavBar />
+            </BrowserRouter>
+          </section>     
       )
     }
   }
