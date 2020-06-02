d476b48fa777139749b80034155a08865f5065e9
Dekpo-Wyna Yologaza <dekpo@me.com>
2020-06-02 03:03:56 +0200

    Travail avec les states dans Todo pour lancer une fonction toggle au clic sur une tache afin de la passer comme terminée ajout d'une variable et d'une condition dans TodoList pour filtrer les taches terminées

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
 class Todo extends React.Component{
+    state = {
+        completed: this.props.task.completed
+    }
+    toggleCompleted = () => {
+        this.setState( prevState => ({
+            completed: !prevState.completed
+        }))
+    }
     render(){
         return(
-            <li className="list-group-item d-flex align-tiems-center">
+            <li className={"list-group-item d-flex align-tiems-center " + (this.state.completed? "bg-success text-white" : null)}>
                 { this.props.task.name }
-                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                <button className={"btn btn-sm ml-auto btn-outline-success " + (this.state.completed? "bg-info text-white" : null)} onClick={() => this.toggleCompleted()}>&#x2713;</button>
             </li>
         )
     }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TodoList = ({tasks}) => (
-    <>
-    <h1 className="m-3">Liste de tâches</h1>
-        <ul className="list-group m-3">
-            {
-                tasks.map((task) => <Todo task={task} key={task.id} />)
-            }
-        </ul>
-    </>
-)
+const TodoList = ({tasks, match}) => {
+    let filteredTasks
+    switch (match.params.filter){
+        case 'completed':
+            filteredTasks = tasks.filter(task => task.completed)
+        break;
+        default:
+            filteredTasks = tasks
+    }
+    if (filteredTasks.length === 0){
+        return(
+            <>
+            <h1 className="m-3">Liste de tâches</h1>
+                <ul className="list-group m-3">
+                <li className="list-group-item d-flex align-tiems-center">Aucune tâche terminée</li>
+                </ul>
+            </>
+        )
+    } else {
+        return(
+            <>
+            <h1 className="m-3">Liste de tâches</h1>
+                <ul className="list-group m-3">
+                    {
+                        filteredTasks.map((task) => <Todo task={task} key={task.id} />)
+                    }
+                </ul>
+            </>
+        )
+    }
+    
+}
 
 export default TodoList
